f028b35295dc30373d817fe2077e6f7db843df06
Mofeoluwa <lijsamobafunmilayo@gmail.com>
2021-01-25 23:11:43 +0100

    leggo

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          100: '#C5360A',
+          200: '#ECEBEB',
+          300: '#F3F2F2'
+        }
+      },
+      fontFamily: {
+        body: ['Nunito']
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
